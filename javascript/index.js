a5747025da10af5022a54f92af7ee3eeffc67e68
Juan Carlos <juancarlos.arias@gmail.com>
2024-01-03 19:27:23 -0600

    Add video

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,25 @@
 import { SplideClass } from "./splide.min.js";
 import { API_URL } from "./contstants.js";
-const splide = new SplideClass(".splide");
+const splide = new SplideClass(".splide", {
+    type: "loop",
+    perPage: 3,
+    perMove: 3,
+    gap: 24,
+    arrows: false,
+    breakpoints: {
+        800: {
+            perPage: 2,
+            perMove: 1,
+            gap: 16,
+        },
+        400: {
+            perPage: 1,
+            perMove: 1,
+            gap: 16,
+            pagination: false,
+        },
+    },
+});
 splide.mount();
 const startDetailsNode = () => {
     const nodes = document.querySelectorAll("details");
